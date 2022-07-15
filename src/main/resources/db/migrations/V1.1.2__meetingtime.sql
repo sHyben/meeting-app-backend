2d2f561ff6ff9884baa106e40d4ee8b3d3fd44c9
FRANCZEL Michal S4Q3A <michal.franczel@erstegroup.com>
2022-07-15 08:43:15 +0200

    Database migration

---
+++ src/main/resources/db/migrations/V1.1.2__meetingtime.sql
@@ -0,0 +1,12 @@
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+02:00";
+
+ALTER TABLE `meetings`
+    ADD COLUMN `anticipated_end_time` datetime DEFAULT NULL;
+
+ALTER TABLE `agenda_points`
+    ADD COLUMN `anticipated_start_time` datetime DEFAULT NULL,
+    ADD COLUMN `anticipated_end_time` datetime DEFAULT NULL;
+
+COMMIT;
