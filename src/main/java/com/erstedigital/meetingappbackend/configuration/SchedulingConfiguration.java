8487f7527d09f72e18cbf53323d701a54100ad69
FRANCZEL Michal S4Q3A <michal.franczel@erstegroup.com>
2022-07-15 08:43:44 +0200

    Scheduling configuration

---
+++ src/main/java/com/erstedigital/meetingappbackend/configuration/SchedulingConfiguration.java
@@ -0,0 +1,20 @@
+package com.erstedigital.meetingappbackend.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+
+@Configuration
+@EnableScheduling
+public class SchedulingConfiguration {
+    @Bean
+    public ThreadPoolTaskScheduler threadPoolTaskScheduler(){
+        ThreadPoolTaskScheduler threadPoolTaskScheduler
+                = new ThreadPoolTaskScheduler();
+        threadPoolTaskScheduler.setPoolSize(5);
+        threadPoolTaskScheduler.setThreadNamePrefix(
+                "ThreadPoolTaskScheduler");
+        return threadPoolTaskScheduler;
+    }
+}
